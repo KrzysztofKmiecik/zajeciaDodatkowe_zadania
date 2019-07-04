d9072fc9a8bf7a21466ee5722b94480fb0b61174
Krzysztof Kmiecik <krzysztof.kmiecik@wp.pl>
2019-07-04 13:49:22 +0200

    zad6MojaData

---
+++ src/main/java/Zad6MojaData/MojaData.java
@@ -0,0 +1,30 @@
+package Zad6MojaData;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class MojaData {
+    private int day;
+    private int month;
+    private int year;
+
+    public String wyswietlDate(char tryb) {
+        switch (tryb) {
+            case 'a': {
+                return new String(String.format("%d.%d.%d", this.day, this.month, this.year));
+            }
+            case 'b': {
+                return new String(String.format("%02d.%02d.%d", this.day, this.month, this.year));
+            }
+            case 'c': {
+                String[] sMonth = {"sty", "lut", "mar", "kwi", "maj", "cze", "lip", "sie", "wrz", "paz", "lis", "gru"};
+                return new String(String.format("%d %s %d", this.day, sMonth[this.month-1], this.year));
+            }
+
+        }
+        return new String("");
+    }
+
+}
